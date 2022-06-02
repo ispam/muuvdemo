f9c7de4ec608c8ff3aca101dfd8b6d27ad792f8f
DiegoU <diego.urrea@thriveglobal.com>
2022-06-02 09:25:21 -0500

    Muuvdemo: Added hilt, okhttp and retrofit libraries

---
+++ app/src/main/java/com/example/muuvdemo/SplashscreenActivity.kt
@@ -0,0 +1,14 @@
+package com.example.muuvdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
